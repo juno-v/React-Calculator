952c5009b0c308da3aca65f28449d409a55377ba
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-03 17:32:53 -0500

    math computatoin is working

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Buttons from "./Components/Buttons"; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    restuls: '', 
+  }
+
+  onClick = button => {
+
+    if(button === "="){
+        this.calculate();
+    }
+
+    else if(button === "C"){
+        this.reset();
+    }
+    else if(button === "CE"){
+        this.backspace();
+    }
+
+    else {
+        this.setState({
+            result: this.state.result + button
+        })
+    }
+};
+
+calculate = () => {
+  let checkResult = ''
+  if(this.state.result.includes('-')){
+      checkResult = this.state.result.replace('-','+')
+  }
+
+  else {
+      checkResult = this.state.result
+  }
+
+  try {
+      this.setState({
+          // eslint-disable-next-line
+          result: (eval(checkResult) || "" ) + ""
+      })
+  } catch (e) {
+      this.setState({
+          result: "error"
+      })
+
+  }
+};
+
+reset = () => {
+  this.setState({
+      result: ""
+  })
+};
+
+backspace = () => {
+  this.setState({
+      result: this.state.result.slice(0, -1)
+  })
+};
+
+  render() {
+    return (
+      <div>
+        <Buttons result={this.state.result} onClick={this.onClick} /> 
+      </div>
+    );
+  }
 }
 
 export default App;
